e0f53011e5477ef3d742623f2ac9d774e35cb185
LucasVicentee <lh201612@gmail.com>
2024-11-13 21:38:12 -0300

    Criação do cabeçalho

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">    
     <title>Cadastro</title>
 </head>
 <body>
-    
+    <header>
+        <nav class="cabecalho">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#cadastro">Cadastro</a></li>
+                <li><a href="#visualizacao">Visualização</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
-?>
